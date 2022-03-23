9c4e7986274f7b0e8e5b6ebea482e3727412dd08
Daniel da Silva <Daniel.da.silva.tj2011@gmail.com>
2022-03-23 19:11:23 -0300

    script

---
+++ script/script.js
@@ -0,0 +1,24 @@
+function calcularSaque(valorSaque) {
+  event.preventDefault(); //Previne da página recarregar ao clicar no botão
+
+  //Limpa os campos
+  document.querySelector("#resultado").innerHTML = "";
+  document.querySelector("#erro").innerHTML = "";
+
+  const notas = [100, 50, 10, 5, 1]; //Notas definidas no desafio
+  let valor = parseInt(valorSaque); //Valor a ser sacado
+
+  //Caso valor inserido seja menor que 1, uma mensagem de erro aparecerá
+  if (valor < 1 || !valor) {
+    document.querySelector("#erro").innerHTML = "Favor Inserir um valor válido para saque!";
+  }
+
+  //Para cada nota, subtrai do valor o máximo de vezes possível
+  for (i = 0; i < notas.length; i++) {
+    if (valor >= notas[i]) {
+      qtdNotas = parseInt(valor / notas[i]) + " nota(s) de " + notas[i] + "\n";
+      document.querySelector("#resultado").innerHTML += (qtdNotas);
+      valor = valor % notas[i];
+    }
+  }
+}
